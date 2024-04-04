23b308fab8d5fd474727fb110264be0c462663ad
Ali Turgut <ali_trgt@outlook.com>
2024-04-04 16:33:29 +0300

    Add files via upload

---
+++ Back-End/SoftwareEngineeringProject kopyası/src/main/java/com/example/SoftwareEngineeringProject/Repository/StudentRepository.java
@@ -0,0 +1,12 @@
+package com.example.SoftwareEngineeringProject.Repository;
+
+import com.example.SoftwareEngineeringProject.Entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+
+public interface StudentRepository extends JpaRepository<Student,Integer> {
+
+
+}
