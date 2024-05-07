70fd8871856bd99ed60cf782a706e2eb94101219
Ali Turgut <ali_trgt@outlook.com>
2024-05-07 17:48:31 +0300

    Add files via upload

---
+++ Back-End/SoftwareEngineeringProject kopyası/src/main/java/com/example/SoftwareEngineeringProject/SoftwareEngineeringProjectApplication.java
@@ -0,0 +1,67 @@
+package com.example.SoftwareEngineeringProject;
+
+import com.example.SoftwareEngineeringProject.Entity.Role;
+import com.example.SoftwareEngineeringProject.Entity.Student;
+import com.example.SoftwareEngineeringProject.Entity.Tutor;
+import com.example.SoftwareEngineeringProject.Entity.User;
+import com.example.SoftwareEngineeringProject.Repository.StudentRepository;
+import com.example.SoftwareEngineeringProject.Repository.TutorRepository;
+import com.example.SoftwareEngineeringProject.Repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.Set;
+
+@SpringBootApplication
+public class SoftwareEngineeringProjectApplication implements CommandLineRunner{
+
+
+	private final TutorRepository tutorRepository;
+	private final StudentRepository studentRepository;
+
+	public SoftwareEngineeringProjectApplication(TutorRepository tutorRepository, StudentRepository studentRepository, UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
+		this.tutorRepository = tutorRepository;
+		this.studentRepository = studentRepository;
+		this.userRepository = userRepository;
+		this.passwordEncoder = passwordEncoder;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareEngineeringProjectApplication.class, args);
+	}
+
+
+	@Override
+	public void run(String... args) throws Exception {
+	}
+
+	private final UserRepository userRepository;
+	private final BCryptPasswordEncoder passwordEncoder;
+	public void CreateDummyData(){
+
+	User user = User.builder()
+			.username("giresun")
+			.password(passwordEncoder.encode("61"))
+			.authorities(Set.of(Role.ROLE_TUTOR))
+			.isEnabled(true)
+			.accountNonLocked(true)
+			.isCredentialsNonExpired(true)
+			.accountNonExpired(true)
+			.build();
+
+			Tutor student = Tutor.builder()
+					.firstName("asda")
+					.lastName("asdsad")
+					.user(user)
+					.subject("math")
+					.build();
+
+			tutorRepository.save(student);
+
+
+
+
+	}
+}
