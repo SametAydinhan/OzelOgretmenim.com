02bdf8eacbd327e83453b2bf925f8f3ac1c1ba10
Samet Aydinhan <samet.aydinhan879@gmail.com>
2024-05-14 15:34:38 +0300

    feat(home): create quick tutor container

--- Front-End/src/pages/Home.js
+++ Front-End/src/pages/Home.js
@@ -1,9 +1,9 @@
-import React from "react";
-import Header from "../components/layout/Header";
-import "./Home.css";
-import Button from "../components/common/Button";
-import { Link } from "react-router-dom";
-import LessonItems from "../components/common/LessonItems/LessonItems";
+import React from 'react';
+import Header from '../components/layout/Header';
+import './Home.css';
+import Button from '../components/common/Button';
+import { Link } from 'react-router-dom';
+import LessonItems from '../components/common/LessonItems/LessonItems';
 
 const Home = () => {
   return (
@@ -19,19 +19,23 @@ const Home = () => {
               </h2>
               <ul>
                 <li>
-                  Kendiniz veya çocuğunuz için en ideal öğretmeni hemen bul.
+                  <i class='fa-solid fa-circle-check'></i> Kendiniz veya
+                  çocuğunuz için en ideal öğretmeni hemen bul.
                 </li>
-                <li>Alanında uzman eğitmenlerden en iyi dersleri al.</li>
+                <li>
+                  <i class='fa-solid fa-circle-check'></i> Alanında uzman
+                  eğitmenlerden en iyi dersleri al.
+                </li>
               </ul>
               <div className='searchbar-container'>
                 <input type='text' placeholder='Hangi dersi almak istersin?' />
                 <Button
                   styled={{
-                    width: "300px",
-                    height: "35px",
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
+                    width: '300px',
+                    height: '35px',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
                   }}
                 >
                   Öğetmen Bul
@@ -40,7 +44,7 @@ const Home = () => {
             </div>
             <div className='image'>
               <img
-                src={require("../assests/images/ozelders.png")}
+                src={require('../assests/images/ozelders.png')}
                 alt='ozel ders almak icin en iyi platform'
               />
             </div>
@@ -49,6 +53,16 @@ const Home = () => {
       </section>
       <section className='lessons'>
         <LessonItems />
+      </section>
+      <section className='quick-tutor-section'>
+        <div className='quick-tutor-container'>
+          <h2>Alanında Uzman Öğretmenleri Keşfet!</h2>
+          <p>
+            Özel ders alanında her yıl onbinlerce öğrenci aradığı öğretmeni
+            buluyor
+          </p>
+          <Button>Hemen Özel Ders Al</Button>
+        </div>
       </section>
     </>
   );
