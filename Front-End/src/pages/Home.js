ea911738bcfcfaa78c808131be89d569dd37fc0b
AliTrgt <ali_trgt@outlook.com>
2024-05-14 22:20:31 +0300

    tüm kod front-end kodu da eklendi hepsi şuan bu branchde

--- Front-End/src/pages/Home.js
+++ Front-End/src/pages/Home.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/layout/Header';
 import './Home.css';
 import Button from '../components/common/Button';
 import { Link } from 'react-router-dom';
 import LessonItems from '../components/common/LessonItems/LessonItems';
+import axios from 'axios'; // Axios kütüphanesini import edin
 
 const Home = () => {
+  // Tutor bilgilerini saklamak için bir state oluşturun
+  const [tutors, setTutors] = useState([]);
+
+  // Component yüklendiğinde backend'den tutor verilerini çekin
+  useEffect(() => {
+    fetchTutors();
+  }, []);
+
+  // Tutor verilerini backend'den çeken fonksiyon
+  const fetchTutors = async () => {
+    try {
+      const response = await axios.get('http://your-backend-url/api/tutors'); // Backend'inizin URL'sini ve API rotasını buraya ekleyin
+      setTutors(response.data); // Backend'den gelen veriyi state'e kaydedin
+    } catch (error) {
+      console.error('Error fetching tutors:', error);
+    }
+  };
+
   return (
     <>
       <Header />
@@ -19,11 +38,11 @@ const Home = () => {
               </h2>
               <ul>
                 <li>
-                  <i class='fa-solid fa-circle-check'></i> Kendiniz veya
+                  <i className='fa-solid fa-circle-check'></i> Kendiniz veya
                   çocuğunuz için en ideal öğretmeni hemen bul.
                 </li>
                 <li>
-                  <i class='fa-solid fa-circle-check'></i> Alanında uzman
+                  <i className='fa-solid fa-circle-check'></i> Alanında uzman
                   eğitmenlerden en iyi dersleri al.
                 </li>
               </ul>
@@ -43,10 +62,7 @@ const Home = () => {
               </div>
             </div>
             <div className='image'>
-              <img
-                src={require('../assests/images/ozelders.png')}
-                alt='ozel ders almak icin en iyi platform'
-              />
+
             </div>
           </div>
         </div>
@@ -62,6 +78,18 @@ const Home = () => {
             buluyor
           </p>
           <Button>Hemen Özel Ders Al</Button>
+        </div>
+      </section>
+      {/* Tutor bilgilerini gösterme */}
+      <section>
+        <div>
+          {tutors.map(tutor => (
+            <div key={tutor.id}>
+              <h3>{tutor.firstName} {tutor.lastName}</h3>
+              <p>Şehir: {tutor.city}</p>
+              {/* Diğer tutor bilgileri */}
+            </div>
+          ))}
         </div>
       </section>
     </>
